d2b59edf549146b085be115f7f10471ac7c405d1
9021315 <144031506+9021315@users.noreply.github.com>
2025-03-11 09:28:39 +0100

    Add files via upload

--- opdracht_login.sql
+++ opdracht_login.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 09 mrt 2025 om 21:31
+-- Gegenereerd op: 11 mrt 2025 om 09:26
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,8 +31,17 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `created_at`, `email`) VALUES
+(6, 'Yahia', '$2y$10$O.jN.S/NZLI25yLnM8UfFe1uXICOHQvKgGFKZcz3SsyTBG1CEC.cu', '2025-03-11 08:12:03', 'yahiaahmad2006@gmail.com'),
+(7, '1234567', '$2y$10$v4qpHyl4IY8NKXphRgeAHulTg3wCjNuS6aSym3KJo6jgaJccIbeL2', '2025-03-11 08:13:25', 'yahiaahmad@gmail.com');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -52,7 +62,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT voor een tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
